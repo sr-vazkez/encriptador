2d95765533163e9f44e041181bb8ef5542ee2693
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-05-12 21:21:57 -0500

    Agregando funcion generar key

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from cryptography.fernet import fernet
 import os
+
+def generateKey():
+     '''
+          Funcion 'Generar key'       
+     '''
+     key = Fernet.generate_key()
+     with open("key.key", "wb") as key_file:
+          key_file.write(key)
+
